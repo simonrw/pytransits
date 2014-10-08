43744a34bbe2e2740bac5d5aa33bb10dab4f1f5e
Simon Walker <s.r.walker101@googlemail.com>
2014-10-08 12:23:39 +0100

    Breaks out variables

--- setup.py
+++ setup.py
@@ -5,15 +5,22 @@ from setuptools.extension import Extension
 from Cython.Distutils import build_ext
 from numpy import get_include
 
+package_name = 'transitgen'
 
-setup(name='transitgen',
+ext_modules = [
+    Extension(
+        "{}._Modelgen".format(package_name),
+        ['{}/_Modelgen.pyx'.format(package_name),
+            'Modelgen/src/GenerateModel.cpp'],
+        language="c++", include_dirs=['Modelgen/include', get_include()]),
+]
+
+setup(name=package_name,
       version='0.0.1',
       author='Simon Walker',
       author_email='s.r.walker101@googlemail.com',
       license='MIT',
-      packages=['transitgen',],
-      ext_modules=[Extension(
-          "transitgen._Modelgen", ['transitgen/_Modelgen.pyx', 'Modelgen/src/GenerateModel.cpp'],
-          language="c++", include_dirs=['Modelgen/include', get_include()]), ],
+      packages=[package_name, ],
+      ext_modules=ext_modules,
       cmdclass={'build_ext': build_ext, }
-)
+      )

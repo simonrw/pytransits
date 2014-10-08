885c1ac73e0fbd42a2da14a1b5c0aa9203eb2745
Simon Walker <s.r.walker101@googlemail.com>
2014-10-08 11:55:32 +0100

    Add extra metadata

--- setup.py
+++ setup.py
@@ -6,8 +6,14 @@ from Cython.Distutils import build_ext
 from numpy import get_include
 
 
-setup(ext_modules=[Extension(
-    "_Modelgen", ['_Modelgen.pyx', 'Modelgen/src/GenerateModel.cpp'],
-    language="c++", include_dirs=['Modelgen/include', get_include()]), ],
-    cmdclass={'build_ext': build_ext, }
-    )
+setup(name='transitgen',
+      version='0.0.1',
+      author='Simon Walker',
+      author_email='s.r.walker101@googlemail.com',
+      license='MIT',
+      packages=['transitgen',],
+      ext_modules=[Extension(
+          "transitgen._Modelgen", ['transitgen/_Modelgen.pyx', 'Modelgen/src/GenerateModel.cpp'],
+          language="c++", include_dirs=['Modelgen/include', get_include()]), ],
+      cmdclass={'build_ext': build_ext, }
+)

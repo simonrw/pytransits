aee0a65bc11e1fe4159606cce5754ed5901b1161
Simon Walker <s.r.walker101@googlemail.com>
2014-10-08 12:23:48 +0100

    Add test install script

---
+++ scripts/test_install.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+
+set -e
+
+setup() {
+    ENVDIR=${PWD}/venv
+    test -d ${ENVDIR} && rm -r ${ENVDIR}
+    conda create --yes --quiet -p ${ENVDIR} pip pytest ipython numpy matplotlib scipy pandas cython
+    source activate ${ENVDIR}
+
+    test -d build && rm -r build
+}
+
+teardown() {
+    source deactivate
+    rm -rf ${ENVDIR}
+}
+
+main() {
+    echo "Setting up"
+    setup >/dev/null
+    echo "Installing"
+    pip install .
+    echo "Running test"
+    python -c 'import transitgen'
+    teardown >/dev/null
+}
+
+trap teardown EXIT
+
+main

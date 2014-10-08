ff9c599b6dd4352b4cd36deccf3d88520942eda8
Simon Walker <s.r.walker101@googlemail.com>
2014-10-08 11:55:10 +0100

    Switch to setuptools

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 
-from distutils.core  import setup
-from distutils.extension import Extension
+from setuptools import setup
+from setuptools.extension import Extension
 from Cython.Distutils import build_ext
 from numpy import get_include
 

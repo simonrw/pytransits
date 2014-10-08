6a8bc526a64b2d578be7c71edaf03802b6e1437d
Simon Walker <s.r.walker101@googlemail.com>
2014-10-08 14:27:17 +0100

    Add accessing attributes

--- testing/test_python_api.py
+++ testing/test_python_api.py
@@ -1,1 +1,10 @@
 from transitgen import Model
+
+model_params = ['period', 'epoch', 'a', 'rs', 'i', 'rp', 'mstar',
+                'c1', 'c2', 'c3', 'c4', 'teff']
+
+def test_model_constructor():
+    m = Model()
+
+    for param in model_params:
+        assert getattr(m, param) == 0
--- transitgen/__init__.py
+++ transitgen/__init__.py
@@ -5,3 +5,6 @@ __all__ = ['Model', ]
 class Model(object):
     def __init__(self):
         self._model = _Modelgen.PyModel()
+
+    def __getattr__(self, attr):
+        return getattr(self._model, attr)

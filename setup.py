53c856f1a335232f005b639cc22e6ccb653a8076
Simon Walker <s.r.walker101@googlemail.com>
2014-10-08 12:34:21 +0100

    Following cython tutorial

--- setup.py
+++ setup.py
@@ -2,18 +2,32 @@
 
 from setuptools import setup
 from setuptools.extension import Extension
-from Cython.Distutils import build_ext
+from Cython.Build import cythonize
 from numpy import get_include
 
 package_name = 'transitgen'
 
 ext_modules = [
-    Extension(
-        "{}._Modelgen".format(package_name),
-        ['{}/_Modelgen.pyx'.format(package_name),
-            'Modelgen/src/GenerateModel.cpp'],
-        language="c++", include_dirs=['Modelgen/include', get_include()]),
-]
+    Extension('{}._Modelgen'.format(package_name),
+              ['{}/_Modelgen.pyx'.format(package_name),
+               'Modelgen/src/GenerateModel.cpp'],
+               language='c++',
+               include_dirs=['Modelgen/include', get_include()],
+               libraries=['c++',])]
+
+
+
+
+
+
+
+
+    # Extension(
+    #     '{}._Modelgen'.format(package_name),
+    #     ['{}/_Modelgen.pyx'.format(package_name),
+    #         'Modelgen/src/GenerateModel.cpp'],
+    #     language='c++', include_dirs=['Modelgen/include', get_include()]),
+
 
 setup(name=package_name,
       version='0.0.1',
@@ -21,6 +35,5 @@ setup(name=package_name,
       author_email='s.r.walker101@googlemail.com',
       license='MIT',
       packages=[package_name, ],
-      ext_modules=ext_modules,
-      cmdclass={'build_ext': build_ext, }
+      ext_modules=cythonize(ext_modules),
       )
